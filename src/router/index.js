88750bcee1504f4d92c150a138414b4b5c24bca8
Rafet Topçu <rafet1998@gmail.com>
2020-12-21 15:32:45 +0300

    route kontrolleri yapıldı

--- src/router/index.js
+++ src/router/index.js
@@ -8,72 +8,135 @@ import EditProfile from "../views/EditProfile.vue";
 import MakeAnAppointment from "../views/MakeAnAppointment.vue";
 import MyAppointments from "../views/MyAppointments.vue";
 import MyDiseases from "../views/MyDiseases.vue";
-
+import VueJwtDecode from "vue-jwt-decode";
 Vue.use(VueRouter);
+
+const ifAuthenticated = (to, from, next) => {
+  const token = Vue.ls.get("token");
+
+  const authUser = Vue.ls.get("token");
+
+  if (to.meta.requiresAuth) {
+    if (authUser) {
+      const { role } = VueJwtDecode.decode(token);
+
+      if (to.meta.roles && to.meta.roles.length > 0) {
+        if (to.meta.roles.includes(role)) {
+          next();
+        } else {
+          next("/");
+        }
+      } else {
+        next();
+      }
+    } else {
+      Vue.ls.clear();
+      next("/login");
+    }
+  } else {
+    next();
+  }
+};
+
+const ifNotAuthenticated = (to, from, next) => {
+  const authUser = Vue.ls.get("token");
+  if (!authUser) {
+    next();
+  } else {
+    next("/profile");
+  }
+};
 
 const routes = [
   {
     path: "/login",
     name: "Login",
     component: Login,
+    beforeEnter: ifNotAuthenticated,
     meta: {
-      layout: null
+      layout: null,
+      requiresAuth: false
     }
   },
   {
     path: "/register",
     name: "Register",
     component: Register,
+    beforeEnter: ifNotAuthenticated,
     meta: {
-      layout: null
+      layout: null,
+      requiresAuth: false
     }
   },
   {
     path: "/home",
     name: "Home",
     component: Home,
+    beforeEnter: ifAuthenticated,
     meta: {
-      layout: "default"
+      layout: "default",
+      requiresAuth: true
     }
   },
   {
     path: "/profile",
     name: "Profile",
     component: Profile,
+    beforeEnter: ifAuthenticated,
     meta: {
-      layout: "default"
+      layout: "default",
+      requiresAuth: true
     }
   },
   {
     path: "/profile/edit",
     name: "EditProfile",
     component: EditProfile,
+    beforeEnter: ifAuthenticated,
     meta: {
-      layout: "default"
+      layout: "default",
+      requiresAuth: true
     }
   },
   {
     path: "/make-an-appointment",
     name: "MakeAnAppointment",
     component: MakeAnAppointment,
+    beforeEnter: ifAuthenticated,
     meta: {
-      layout: "default"
+      layout: "default",
+      requiresAuth: true
     }
   },
   {
     path: "/my-appointments",
     name: "MyAppointments",
     component: MyAppointments,
+    beforeEnter: ifAuthenticated,
     meta: {
-      layout: "default"
+      layout: "default",
+      requiresAuth: true
     }
   },
   {
     path: "/my-diseases",
     name: "MyDiseases",
     component: MyDiseases,
+    beforeEnter: ifAuthenticated,
     meta: {
-      layout: "default"
+      layout: "default",
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/*",
+    name: "all",
+    beforeEnter: (to, from, next) => {
+      next("/login");
+    },
+    meta: {
+      layout: "default",
+      requiresAuth: true
     }
   }
 ];
